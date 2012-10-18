6c62a81c48650c6a78f4f6e5ea47f2bf653f0b15
dfjacobs <dfjacobs@sshores.com>
2012-10-18 00:43:49 -0600

    Removed API tester and added methods for the properties resource.

--- src/test/java/com/radishsystems/choiceview/webapi/ChoiceViewSessionTest.java
+++ src/test/java/com/radishsystems/choiceview/webapi/ChoiceViewSessionTest.java
@@ -37,6 +37,15 @@ public class ChoiceViewSessionTest {
 			this.href = href;
 		}
 	}
+	static class Property {
+		public String name;
+		public String value;
+		public Property() { this("", ""); }
+		public Property(String name, String value) {
+			this.name = name;
+			this.value = value;
+		}
+	}
 	static class Session {
 		public int sessionId;
 		public String callerId;
@@ -44,8 +53,8 @@ public class ChoiceViewSessionTest {
 		public String status;
 		public String networkQuality;
 		public String networkType;
-		public Map<String, String> properties;
-		public List<Link> links;
+		final public Map<String, String> properties;
+		final public List<Link> links;
 		public Session() {
 			sessionId = expectedSessionId;
 			status = "connected";
@@ -54,6 +63,16 @@ public class ChoiceViewSessionTest {
 			properties = new HashMap<String, String>();
 			properties.put("TestKey1", "TestValue");
 			properties.put("TestKey2", "TestValue");
+			links = new ArrayList<Link>();
+		}
+	}
+	static class Payload {
+		final public Map<String, String> properties;
+		final public List<Link> links;
+		public Payload() {
+			properties = new HashMap<String, String>();
+			properties.put("TestKey1", "UpdatedTestValue");
+			properties.put("TestKey2", "UpdatedTestValue");
 			links = new ArrayList<Link>();
 		}
 	}
@@ -116,6 +135,8 @@ public class ChoiceViewSessionTest {
 					session.links.add(new Link("self",selfUri));
 					session.links.add(new Link(ChoiceViewSession.ControlMessageRel,
 							          selfUri + "/controlmessage"));
+					session.links.add(new Link(ChoiceViewSession.PayloadRel,
+					          selfUri + "/properties"));
 					response.setEntity(new StringEntity(mapper.writeValueAsString(session),
 							ContentType.create("application/json", "utf-8")));
 
@@ -147,6 +168,8 @@ public class ChoiceViewSessionTest {
 				session.links.add(new Link("self", sessionUri));
 				session.links.add(new Link(ChoiceViewSession.ControlMessageRel,
 						          sessionUri + "/controlmessage"));
+				session.links.add(new Link(ChoiceViewSession.PayloadRel,
+				          sessionUri + "/properties"));
 				if(request.containsHeader("ACCEPT")) {
 					String acceptHeader = request.getFirstHeader("ACCEPT").getValue();
 					if(acceptHeader != null && !acceptHeader.toUpperCase().contains("JSON")) {
@@ -155,6 +178,7 @@ public class ChoiceViewSessionTest {
 				}
 				response.setEntity(new StringEntity(mapper.writeValueAsString(session),
 						ContentType.create("application/json", "utf-8")));
+				return;
 			}
 			if(method.equals("POST")) {
 				String content = null;
@@ -183,6 +207,7 @@ public class ChoiceViewSessionTest {
 				} else {
 					throw new ProtocolException("No content in POST request!");
 				}
+				return;
 			}
 		}
 	};
@@ -190,8 +215,61 @@ public class ChoiceViewSessionTest {
 	private HttpRequestHandler propertiesHandler = new HttpRequestHandler() {
 		public void handle(HttpRequest request, HttpResponse response,
 				HttpContext context) throws HttpException, IOException {
-			// TODO Auto-generated method stub
-			
+			String method = request.getRequestLine().getMethod().toUpperCase();
+			if(method.equals("PUT")) {
+				throw new MethodNotSupportedException("PUT request not supported for properties");
+			}
+			if(method.equals("DELETE")) {
+				throw new MethodNotSupportedException("DELETE request not supported for properties");
+			}
+			if(method.equals("GET")) {
+				String payloadUri = "http://" + testServer.getServiceAddress().getHostName() + ":" +
+						testServer.getServiceAddress().getPort() + request.getRequestLine().getUri();
+				String sessionUri = payloadUri.replace("/properties", "");
+				Payload payload = new Payload();
+				payload.links.add(new Link("self", payloadUri));
+				payload.links.add(new Link(ChoiceViewSession.SessionRel,
+						          sessionUri));
+				if(request.containsHeader("ACCEPT")) {
+					String acceptHeader = request.getFirstHeader("ACCEPT").getValue();
+					if(acceptHeader != null && !acceptHeader.toUpperCase().contains("JSON")) {
+						throw new ProtocolException(acceptHeader + " type not supported!");
+					}
+				}
+				response.setEntity(new StringEntity(mapper.writeValueAsString(payload),
+						ContentType.create("application/json", "utf-8")));
+            	response.setStatusCode(200);
+				return;
+			}
+			if(method.equals("POST")) {
+				String content = null;
+				if(request instanceof HttpEntityEnclosingRequest) {
+	                HttpEntity entity = ((HttpEntityEnclosingRequest) request).getEntity();
+	                content = EntityUtils.toString(entity);
+					if(content == null) {
+						throw new ProtocolException("Cannot retrieve content from POST request!");
+					}
+	                if(request.containsHeader("Content-Type")) {
+	                	String value = request.getFirstHeader("Content-Type").getValue().toLowerCase();
+	                	if(value == null) {
+	    					throw new ProtocolException("No content type specified in POST request!");
+	                	}
+	                	if(value.contains("application/json")) {
+	        				List<Property> params = mapper.readValue(content,
+	    							new TypeReference<List<Property>>() {});
+	        				if(params != null) {
+			                	response.setStatusCode(200);
+			                	return;
+	        				}
+	                	}
+						throw new ProtocolException("No property list in POST request!");	                	
+	                } else {
+		                throw new ProtocolException("No content-type header in POST request!");
+	                }
+				} else {
+					throw new ProtocolException("No content in POST request!");
+				}
+			}
 		}
 	};
 	
@@ -221,10 +299,12 @@ public class ChoiceViewSessionTest {
 		mapper = new ObjectMapper();
 		
 		testServer = new LocalTestServer(null, null);
+		String baseUri = "/ivr/api/session/";
+		String sessionUri = baseUri + expectedSessionId;
 		testServer.register("/ivr/api/sessions", sessionsHandler);
-		testServer.register("/ivr/api/session/" + expectedSessionId, sessionHandler);
-		testServer.register("/ivr/api/session/" + expectedSessionId + "/controlmessage",
-				            controlMessageHandler);
+		testServer.register(sessionUri, sessionHandler);
+		testServer.register(sessionUri + "/controlmessage", controlMessageHandler);
+		testServer.register(sessionUri + "/properties", propertiesHandler);
 		testServer.start();
 		
 		testSession = new ChoiceViewSession(testServer.getServiceAddress().getHostName(),
@@ -296,6 +376,46 @@ public class ChoiceViewSessionTest {
 		assertEquals(expectedControlMessage, actualControlMessage);
 	}
 
+	@Test
+	public void testUpdateProperties() {
+		final Map<String, String> expectedProperties = new HashMap<String, String>();
+		expectedProperties.put("TestKey1", "UpdatedTestValue");
+		expectedProperties.put("TestKey2", "UpdatedTestValue");
+		
+		assertNull(testSession.UpdateProperties());
+		assertTrue(testSession.StartSession(expectedCallerId, expectedCallId));
+		Map<String, String> actualProperties = testSession.UpdateProperties();
+		assertEquals(expectedProperties, actualProperties);
+	}
+
+	@Test
+	public void testAddProperties() {
+		final Map<String, String> duplicateProperties = new HashMap<String, String>();
+		duplicateProperties.put("TestKey1", "UpdatedTestValue");
+		duplicateProperties.put("TestKey2", "UpdatedTestValue");
+		
+		final Map<String, String> goodProperties = new HashMap<String, String>();
+		goodProperties.put("TestKey3", "UpdatedTestValue");
+		goodProperties.put("TestKey4", "UpdatedTestValue");
+
+		assertFalse(testSession.AddProperties(duplicateProperties));
+		assertTrue(testSession.StartSession(expectedCallerId, expectedCallId));
+		assertFalse(testSession.AddProperties(duplicateProperties));
+		assertTrue(testSession.AddProperties(goodProperties));
+	}
+
+	@Test
+	public void testAddProperty() {
+		String badName = "TestKey1";
+		String goodName = "TestKey3";
+		String testValue = "TestValue";
+		
+		assertFalse(testSession.AddProperty(goodName, testValue));
+		assertTrue(testSession.StartSession(expectedCallerId, expectedCallId));
+		assertFalse(testSession.AddProperty(badName, testValue));
+		assertTrue(testSession.AddProperty(goodName, testValue));
+	}
+	
 	@After
 	public void tearDown() throws Exception {
 		testServer.stop();
